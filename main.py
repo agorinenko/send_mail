0eac516dd878726635b1d6e984c711b90218044c
anton <anton.gorinenko@gmail.com>
2020-09-08 14:40:35 +0300

    Docker

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 
 env.read_envfile()
-
+ENV = env.str('ENV')
+IS_DEV = (ENV == "DEV")
 GMAIL_USER = env.str('GMAIL_USER')
 GMAIL_PASSWORD = env.str('GMAIL_PASSWORD')
 HOST_NAME = env.str('HOST_NAME')
@@ -21,20 +22,23 @@ TO = env.str('TO')
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://127.0.0.1",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-]
+BASE_URL = "/api/v1"
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+if IS_DEV:
+    origins = [
+        "http://localhost",
+        "http://127.0.0.1",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 class Mail(BaseModel):
@@ -42,7 +46,7 @@ class Mail(BaseModel):
     body: str
 
 
-@app.post("/send-email")
+@app.post(f"{BASE_URL}/send-email")
 async def send_email(mail: Mail = Body(None, embed=True)):
     message = EmailMessage()
     message["From"] = FROM
@@ -62,5 +66,5 @@ async def send_email(mail: Mail = Body(None, embed=True)):
     })
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" and IS_DEV:
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
